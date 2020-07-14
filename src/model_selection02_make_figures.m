4431aeda1a8bdb2b2ecf33a549ae922a5352ddf1
nlammers371 <nlammers@berkeley.edu>
2020-07-14 12:35:28 -0700

    Finished first draft of figure 4 script

--- src/model_selection02_make_figures.m
+++ src/model_selection02_make_figures.m
@@ -20,19 +20,19 @@ load([DataPath 'waiting_time_struct.mat'])
 
 % set basic plot parameters
 t_max = 60;
-ylimTrace = [-0.5 6.5];
+ylimTrace = [-0.5 8];
 n_bound_vec = 0:n_bcd_sites;
 
 % sim name cell
 sim_name_cell = {waiting_time_struct.name};
 
 % define colors
-purple = [171 133 172]/255;
+purple = brighten([171 133 172]/255,.5);
 blue = [190 201 224]/255;
 red = [246 141 100]/255;
 green = [203 220 170]/255;
 gray = [0.7020    0.7020    0.7020];
-cmap1 = [green ; blue ;red];
+% cmap1 = [green ; blue ;red];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % (1) Make figure illustrating passage time concept
@@ -42,9 +42,9 @@ cmap1 = [green ; blue ;red];
 rateLim_sim_index = find(contains(sim_name_cell,'2rate-limiting'));
 %
 %  extract n bound vec
-rateLim_sim_sub_index = 2;
+rateLim_sim_sub_index = length(waiting_time_struct(rateLim_sim_index).off_waiting_times_ideal);
 % plot results of stochastic simulations
-trace_index = 6;
+trace_index = 4;
 
 state_fig = figure;%('Position',[100 100 1024 512]);
 hold on
@@ -62,12 +62,12 @@ stairs(viterbi_time/60, trace_raw,'Color',purple,'LineWidth',1);
 stairs(viterbi_time/60, viterbi_fit,'Color','k','LineWidth',1.5);
 
 ylim(ylimTrace)
-xlim([0 t_max])
+xlim([0 t_max/2])
 ylabel('transcription rate')
 xlabel('time (minutes)')
 box on
 % legend('raw trace','2 state fit')
-set(gca,'Fontsize',14,'YTick',n_bound_vec)
+set(gca,'Fontsize',14,'YTick',0:7)
 p = plot(0,0);
 ax = gca;
 ax.YColor = 'black';
@@ -116,14 +116,17 @@ end
 
 
 
+
+
+
 offset = 2;
 rl_hist_fig = figure;
 cmap1_raw = brewermap([],'Pastel2');
 cmap2_raw = brewermap([],'Set2');
 start1 = brighten(cmap1_raw(2,:),-0.8);
-end1 = 0.5*brighten(cmap1_raw(2,:),0.8)+[.5 .5 .5];
+end1 = 0.3*brighten(cmap1_raw(2,:),0.8)+[.7 .7 .7];
 start2 = brighten(cmap2_raw(2,:),-.8);
-end2 = .5*brighten(cmap2_raw(2,:),.8)+[.5 .5 .5];
+end2 = .3*brighten(cmap2_raw(2,:),.8)+[.7 .7 .7];
 
 
 cmap1 = interp1([1,length(rateLim_sim_indices)],[start1 ; end1],linspace(1,length(rateLim_sim_indices),length(rateLim_sim_indices)+offset));
@@ -135,13 +138,19 @@ pl = [];
 lgd_str = {};
 for i = fliplr(1:length(rateLim_sim_indices))
   p_vec = rl_fit_struct(i).p_vec;  
-  bar(wt_centers,p_vec,1,'FaceColor',cmap1(i+offset/2,:),'FaceAlpha',.7,'EdgeAlpha',0.3);   
+  if i == length(rateLim_sim_indices)
+    bar(wt_centers,p_vec,1,'FaceColor',cmap1(i+offset/2,:),'FaceAlpha',1,'EdgeAlpha',0.3);   
+  elseif i > 1
+    bar(wt_centers,p_vec,1,'FaceColor',cmap1(i+offset/2,:),'FaceAlpha',.5,'EdgeAlpha',0.3);  
+  else
+    bar(wt_centers,p_vec,1,'FaceColor',cmap1(i+offset/2,:),'FaceAlpha',0.5,'EdgeAlpha',0.3);  
+  end
   pd_vec = rl_fit_struct(i).pd_vec;
   pl = [pl plot(wt_centers,pd_vec,'Color',cmap2(i+offset/2,:),'LineWidth',2)]; 
   if i == 1
-    lgd_str = [lgd_str{:} {[num2str(n_step_vec(i)) ' rate-limiting step']}];
+    lgd_str = [lgd_str{:} {[num2str(n_step_vec(i)) ' step']}];
   else
-    lgd_str = [lgd_str{:} {[num2str(n_step_vec(i)) ' rate-limiting steps']}];
+    lgd_str = [lgd_str{:} {[num2str(n_step_vec(i)) ' steps']}];
   end
 end
 % for i = 1:length(rateLim_sim_indices)
@@ -163,7 +172,6 @@ StandardFigurePBoC(p,gca);
 rl_hist_fig.InvertHardcopy = 'off';
 saveas(rl_hist_fig,[FigurePath 'rateLim_wt_hist.png'])
 saveas(rl_hist_fig,[FigurePath 'rateLim_wt_hist.pdf'])
-
 
 
 coop_sim_index = find(ismember(sim_name_cell,{'koff-mediated cooperativity'}));
@@ -213,8 +221,53 @@ coop_hist_fig.InvertHardcopy = 'off';
 saveas(coop_hist_fig,[FigurePath 'coop_wt_hist.png'])
 saveas(coop_hist_fig,[FigurePath 'coop_wt_hist.pdf'])
 
+%%
+xmax = 5.5;
+ymax = xmax;
+% make fano factor figure
+marker_cell = {'o','o','^','d','s'};
+plot_indices = [2 rateLim_sim_indices];
+plot_colors = [blue ; cmap2(2:end-1,:)];
 
-%% make fano factor figure
+fano_struct = struct;
 
+fano_fig = figure;
+hold on
+plot(linspace(0,max([xmax,ymax])),linspace(0,max([xmax,ymax])),'--','Color','k','LineWidth',1.5);
+iter = 1;
+for p = plot_indices
+  wt_cell = waiting_time_struct(p).off_waiting_times_ideal;
+  mean_vec = NaN(size(wt_cell));
+  std_vec = NaN(size(wt_cell));
+  for w = 1:length(wt_cell)
+    mean_vec(w) = mean(wt_cell{w});
+    std_vec(w) = std(wt_cell{w});
+  end
+  fano_struct(iter).mean_vec = mean_vec / mean_vec(1);
+  fano_struct(iter).std_vec = std_vec / mean_vec(1);  
+    
+  % plot
+  if iter~=2
+    scatter(mean_vec/ mean_vec(1),std_vec/ mean_vec(1),60,marker_cell{iter},'MarkerFaceColor',...
+      plot_colors(iter,:),'MarkerEdgeColor','k','MArkerFaceAlpha',0.8)
+  else
+    scatter(mean_vec/ mean_vec(1),std_vec/ mean_vec(1),60,marker_cell{iter},'MarkerFaceColor',...
+      plot_colors(iter,:),'MarkerEdgeColor','k','MArkerFaceAlpha',0.5)
+  end
+  iter = iter + 1;
+end
 
-
+xlim([0.5 xmax])
+ylim([0 ymax])
+% grid on
+set(gca,'Fontsize',14)
+ylabel('standard deviation')
+xlabel('mean passage time from 0->6')
+p = plot(0,0);
+ax = gca;
+ax.YColor = 'black';
+ax.XColor = 'black';
+StandardFigurePBoC(p,gca);
+fano_fig.InvertHardcopy = 'off';
+saveas(fano_fig,[FigurePath 'fano_scatter.png'])
+saveas(fano_fig,[FigurePath 'fano_scatter.pdf'])
